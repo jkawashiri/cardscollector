c17711f462a0075a7cd58e09d806d0ffa0f85ff6
jkawashiri <jkawashiri5@gmail.com>
2023-09-11 11:16:42 -0700

    initial commit

---
+++ main_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+cards = [
+    {'name': 'Lebron James', 'year': 2003, 'brand/type': 'Topps Chrome'},
+    {'name': 'Shohei Ohtani', 'year': 2018, 'brand/type': 'Bowman Chrome Autograph'},
+    {'name': 'Tiger Woods', 'year': 2001, 'brand/type': 'SP Authentic Autograph'},
+]
+
+# Create your views here.
