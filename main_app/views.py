63149dc02f53d06ed235f91d14f7d68d1b21d3b7
jkawashiri <jkawashiri5@gmail.com>
2023-09-11 11:39:14 -0700

    adds navbar for about and all cards pages and displays details for each page

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
 cards = [
-    {'name': 'Lebron James', 'year': 2003, 'brand/type': 'Topps Chrome'},
-    {'name': 'Shohei Ohtani', 'year': 2018, 'brand/type': 'Bowman Chrome Autograph'},
-    {'name': 'Tiger Woods', 'year': 2001, 'brand/type': 'SP Authentic Autograph'},
+    {'name': 'Lebron James', 'year': 2003, 'type': 'Topps Chrome'},
+    {'name': 'Shohei Ohtani', 'year': 2018, 'type': 'Bowman Chrome Autograph'},
+    {'name': 'Tiger Woods', 'year': 2001, 'type': 'SP Authentic Autograph'},
 ]
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cards_index(request):
+    return render(request, 'cards/index.html', {
+        'cards': cards
+    })
